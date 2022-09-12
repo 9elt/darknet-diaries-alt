ed2100d78833ff1c936c21e00e538f4cba901c1f
n-e-l-t <nelt.primary@gmail.com>
2022-09-12 23:39:13 +0200

    main js decent

--- js/main.js
+++ js/main.js
@@ -1,314 +1,255 @@
-const TARGET_URL = 'https://darknetdiaries.com';
-const EPISODE_PAGE = document.querySelector('template.episode').innerHTML;
-const LIST_PAGE = document.querySelector('template.list').innerHTML;
-const EP_PREVIEW = document.querySelector('template.ep-preview').innerHTML;
+const target_url = 'https://darknetdiaries.com';
+
+const episode_template = document.querySelector('template.episode').innerHTML;
+const list_template = document.querySelector('template.list').innerHTML;
+const preview_template = document.querySelector('template.ep-preview').innerHTML;
 
 fakeRouter();
 listenUrlChanges();
 
+//  fake router uses hashes to render the page
+//  as there is no actual routing
 async function fakeRouter() {
 
-    let EP = window.location.hash.substring(1);
-
-    if (EP != '' && parseInt(EP)) {
-        renderEpisode(EP); 
-        bannerAnimation();
-    } else {
-        if (EP != '') {
-            renderList(EP);
-        } else {
-            renderList('');
-        }
-        bannerAnimation();
-    }
-
+    const EP = window.location.hash.substring(1);
+    parseInt(EP) ? renderEpisode(EP): renderList(EP);
 }
 
 async function listenUrlChanges() {
 
     window.addEventListener('hashchange', () => {
-        
+        window.scrollTo({top: 0, behavior: 'smooth'});
         fakeRouter();
-
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-
-    } );
+    });
 }
 
-//  this will work only if the target
-//  server has all origins allowed
+//  this will work only if the target server has
+//  all origins allowed
+async function fetchRequest(url) {
+
+    let page = await fetch(target_url + '/' + url);
+    page = await page.text();
+
+    return page;
+}
+
 async function scrapePage(url) {
 
-    let page = fetch(TARGET_URL + '/' + url);
-    page = (await page).text();
+    let str = await fetchRequest(url);
 
-    return await page;
-}
+    //  the scraped page is created without appending it to the page,
+    //  in order to be able to use dom element methods it.
+    let scraped = document.createElement('div');
 
-//  load episode
-function loadEpisodePage() {
+    //  replacing the images src attribute prevents them
+    //  from being donwloaded when loading into the dom
+    str = str.replaceAll('src="', 'data-src="');
+    scraped.innerHTML = str;
 
-    //  loads the hmtl for base episode page
-    document.getElementById('main').innerHTML = EPISODE_PAGE;
+    return scraped;
 }
 
 async function renderEpisode(ep) {
 
-    loadEpisodePage();
+    document.getElementById('main').innerHTML = episode_template;
 
-    //  the scraped page is loaded without appending it to the page,
-    //  in order to be able to use dom element methods it.
+    // SCRAPING
 
-    //  "scr_variable" is the scraped element while
-    //  "variable" is the element data is appended to
+    const scraped = await scrapePage(`episode/${ep}/`);
 
-    let page = await scrapePage('episode/' + ep + '/');
+    //  the first script tag in the episode description is the player configuration
+    //  containing url to the podcast mp3
+    let scr_description = scraped.querySelector('article.single-post');
+    let scr_player_config = scr_description.querySelector('script');
+    scr_player_config = scr_player_config.innerHTML.replace('window.playerConfiguration = ', '');
+    scr_player_config = JSON.parse(scr_player_config);
 
-    let scraped = document.createElement('div');
-    //  replacing the images src attribute prevents them
-    //  from being donwloaded when loading into the dom
-    page = page.replaceAll('src="', 'data-src="');
-    scraped.innerHTML = page;
-
-    //  loading player configuration
-    let scr_desc = scraped.querySelector('article.single-post');
-
-    let scr_conf = scr_desc.querySelector('script');
-    scr_conf = scr_conf.innerHTML.replace('window.playerConfiguration = ', '');
-    scr_conf = JSON.parse(scr_conf);
-
-    //  let cover = scr_conf.episode.coverUrl
-    //  let mp3 = scr_conf.episode.media
-    //  let duration = scr_conf.episode.subtitle
-
-    let pod = document.querySelector('darknet-diaries').shadowRoot;
-    let podaudio = pod.querySelector('audio');
-    podaudio.src = scr_conf.episode.media.mp3;
-
-    //  images
     let scr_img = scraped.querySelector('img');
-    let bg_image = document.getElementById('ep-bg-image');
-    let cover_image = document.getElementById('ep-cover-image');
 
-    bg_image.src = replaceUrl(scr_img.dataset.src);
-    cover_image.src = replaceUrl(scr_img.dataset.src);
+    const scr_ep_info = scraped.querySelector('section.hero--single>.wrap');
+    let scr_title = scr_ep_info.querySelector('h1').innerHTML;
+    let scr_date = scr_ep_info.querySelector('p').innerHTML;
+    let scr_cats = scr_ep_info.querySelectorAll('a');
 
-    //  position images
-    cover_image.onload = function() {
-        if (this.width/this.height > 1) {
-            let margin = (1 - this.width/this.height)*50;
-            this.style = 'margin-left: ' + margin + '%';
-        }
+    //  removing unwanted elements
+    scr_description.querySelector('.nextprevious').remove();
+    scr_description.querySelector('p>img').remove(); // first cover image
+    scr_description.querySelector(`p>a[href="/transcript/${ep}"]`).remove();
+    scr_description.querySelector(`p>a[href="/transcript/${ep}"]`).remove();
+
+    let scr_desc_imgs = scr_description.querySelectorAll('img');
+    
+    //  removing empty paragraphs
+    while (scr_description.querySelector('p').innerHTML == '') {
+        scr_description.querySelector('p').remove();
     }
 
-    let scr_info = scraped.querySelector('section.hero--single>.wrap');
+    //  first description paragraph
+    let scr_small_desc = scr_description.querySelector('p');
+    scr_small_desc = scr_small_desc.innerHTML;
 
-    //  title
-    let scr_title = scr_info.querySelector('h1').innerHTML;
+    scr_small_desc = limitParagraph(scr_small_desc, 210);
+
+    // RENDERING
+
+    const podcast_root = document.querySelector('darknet-diaries').shadowRoot;
+    let podcast = podcast_root.querySelector('audio');
+    let bg_image = document.getElementById('ep-bg-image');
+    let cover_image = document.getElementById('ep-cover-image');
+    let cats = document.getElementById('ep-categories');
+    let previous = document.getElementById('ep-prev');
+    let next = document.getElementById('ep-next');
+    let last_ep = document.getElementById('last-episode').value;
+    let desc = document.getElementById('ep-desc');
+    let desc_small = document.getElementById('ep-desc-small');
+    let transcript = document.getElementById('transcript');
+
+    //  loading podcast audio
+    podcast.src = scr_player_config.episode.media.mp3;
+
+    //  loading images
+    bg_image.src = replaceUrl(scr_img.dataset.src);
+    cover_image.src = replaceUrl(scr_img.dataset.src);    
+    centerImage(cover_image);
+
+    scr_desc_imgs.forEach(desc_img => {
+        if (! desc_img.src || desc_img.src == '') {
+            desc_img.parentElement.classList.add('imajeff');
+            desc_img.src = replaceUrl(desc_img.dataset.src);
+        }
+    });
+
+    //  episode main info
     document.getElementById('ep-title').innerHTML = scr_title.split(':')[1];
     document.getElementById('ep-number').innerHTML = scr_title.split(':')[0];
-
-    //  date
-    let scr_date = scr_info.querySelector('p').innerHTML;
     document.getElementById('ep-date').innerHTML = scr_date;
+    desc_small.innerHTML = scr_small_desc;
 
-    //  categories
-    let scr_cats = scr_info.querySelectorAll('a');
-    let cats = document.getElementById('ep-categories');
+    //  episode categories
     scr_cats.forEach(scr_cat => {
         let cat = document.createElement('li');
         cat.innerHTML = scr_cat.querySelector('div').innerHTML;
         cats.append(cat);
     });
 
-    //  next and previous buttons
-    let previous = document.getElementById('ep-prev');
-    let next = document.getElementById('ep-next');
-
+    //  next and previous links
     ep_prev = parseInt(ep) - 1;
     ep_next = parseInt(ep) + 1;
-
-    if (ep_prev > 0) {
-        previous.href = '#' + ep_prev;
-    } else {
-        previous.classList.add('disabled');
-    }
-
-    //  get last episode number from input value
-    let last_ep = document.getElementById('last-episode').value;
-    if (last_ep == '') last_ep = 123;
-
-    if (ep_next <= parseInt(last_ep)) {
-        next.href = '#' + ep_next;
-    } else {
-        next.classList.add('disabled');
-    }
-
-    //  description
-    let desc = document.getElementById('ep-desc');
-
-    //  removing unwanted elements
-    scr_desc.querySelector('.nextprevious').remove();
-    scr_desc.querySelector('p>img').remove(); // cover image
-    scr_desc.querySelector('p>a[href="/transcript/' + ep + '"]').remove();
-    scr_desc.querySelector('p>a[href="/transcript/' + ep + '"]').remove();
-
-    //  loading desc images
-    let desc_imgs = scr_desc.querySelectorAll('img');
-    desc_imgs.forEach(desc_img => {
-        if (!desc_img.src || desc_img.src == '') {
-            desc_img.parentElement.classList.add('imajeff');
-            desc_img.src =  replaceUrl(desc_img.dataset.src);
-        }
-    });
-
-    let desc_small = document.getElementById('ep-desc-small');
-    //  removing empty paragraphs
-    while (scr_desc.querySelector('p').innerHTML == '') {
-        scr_desc.querySelector('p').remove();
-    }
-    //  getting description first paragraph
-    let scr_desc_small = scr_desc.querySelector('p');
-    scr_desc_small = scr_desc_small.innerHTML;
-    if (scr_desc_small.length > 210) scr_desc_small = scr_desc_small.substring(0, 210) + '...';
-    desc_small.innerHTML = scr_desc_small;
+    ep_prev > 0 ? previous.href = '#' + ep_prev : previous.classList.add('disabled');
+    ep_next <= last_ep ? next.href = '#' + ep_next : next.classList.add('disabled');
 
     //  appending description
-    desc.append(scr_desc);
+    desc.append(scr_description);
 
-    let transcript = document.getElementById('transcript');
-
+    //  toggle transcript
     transcript.addEventListener('click', async function () {
 
-        if (isActive(this)){
+        toggleActive(this);
+        desc.innerHTML = '';
 
-            this.innerHTML = 'Read full transcript';
-            this.classList.remove('active');
-
-            desc.innerHTML = '';
-            desc.append(scr_desc);
-        
-        } else {
+        if (isActive(this)) {
 
             this.innerHTML = 'Read description';
-            this.classList.add('active');
-    
-            let page = await scrapePage('transcript/' + ep);
-    
-            let scraped = document.createElement('div');
-            page = page.replaceAll('src="', 'data-src="');
-            scraped.innerHTML = page;
-    
+
+            const scraped = await scrapePage(`transcript/${ep}`);
             let scr_transcript = scraped.querySelector('article.single-post pre');
-    
-            desc.innerHTML = '';
+
             desc.append(scr_transcript);
 
+        } else {
+
+            this.innerHTML = 'Read full transcript';
+            desc.append(scr_description);
         }
     });
 
     toggleDescription();
     parallaxAnimation(window.outerHeight);
-}
-
-//  load episode list
-async function loadListPage() {
-
-    //  loads the hmtl for base episode page
-    document.getElementById('main').innerHTML = LIST_PAGE;
+    bannerAnimation();
 }
 
 async function renderList(pagination) {
 
-    loadListPage();
+    document.getElementById('main').innerHTML = list_template;
 
-    if (pagination != '') pagination = pagination + '/'
-    let page = await scrapePage('episode/' + pagination);
+    if (pagination != '') pagination = pagination + '/';
 
-    let scraped = document.createElement('div');
-    page = page.replaceAll('src="', 'data-src="');
-    scraped.innerHTML = page;
+    const scraped = await scrapePage(`episode/${pagination}`);
 
     //  pagination links
-    let scr_page_links = scraped.querySelectorAll('.pagination a');
+    const scr_page_links = scraped.querySelectorAll('.pagination a');
     scr_page_links.forEach(scr_page_link => {
 
-        let page_link = document.createElement('a');
+        let link_url = scr_page_link.href.split('/episode/')[1];
+        link_url = link_url.split('"')[0];
 
-        let page_url = scr_page_link.href;
-        page_url = page_url.split('/episode/')[1];
+        // keep only pages numeric links
+        if (link_url != undefined && parseInt(scr_page_link.innerHTML)) {
 
-        if (page_url != undefined && parseInt(scr_page_link.innerHTML)) {
+            let page_link = document.createElement('a');
 
-            page_url = page_url.split('"')[0];
+            //  current page is active
+            if (link_url + '/' == pagination) page_link.classList.add('active');
 
-            if (page_url == pagination || page_url + '/' == pagination) {
-                page_link.classList.add('active');
-            }
-
-            page_link.href = '#' + page_url;
-
+            page_link.href = '#' + link_url;
             page_link.innerHTML = scr_page_link.innerHTML;
 
-            document.querySelector('.pages-top').append(page_link.cloneNode(true));
+            document.querySelector('.pages-top').append( page_link.cloneNode(true) );
             document.querySelector('.pages-bottom').append(page_link);
         }
     });
 
     //  episode preview
     const container = document.getElementById('container');
-    let scr_list = scraped.querySelectorAll('.listing>.wrap>article');
+    const scr_list = scraped.querySelectorAll('.listing>.wrap>article');
 
     scr_list.forEach(scr_entry => {
 
         let preview = document.createElement('div');
-        preview.innerHTML = EP_PREVIEW;
+        preview.innerHTML = preview_template;
+
+        //  SCRAPING
+
+        let scr_url = scr_entry.querySelector('.post__content a').href;
+        let scr_description = scr_entry.querySelector('.post__description').innerHTML;
+        let scr_date = scr_entry.querySelector('.post__date').innerHTML;
+        let scr_title = scr_entry.querySelector('.post__title>a').innerHTML;
+        let scr_image_url = scr_entry.querySelector('.post__image').style.backgroundImage;
+
+        scr_image_url = scr_image_url.split('url("')[1];
+        scr_image_url = scr_image_url.split('")')[0];
+
+        scr_description = limitParagraph(scr_description, 100);
+
+        scr_url = scr_url.split('/episode/')[1];
+        scr_url = scr_url.split('/')[0];
+
+        //  RENDERING
 
         //  cover image
         let image = preview.querySelector('img');
-        let image_url = scr_entry.querySelector('.post__image').style.backgroundImage;
-        image_url = image_url.split('url("')[1];
-        image_url = image_url.split('")')[0];
-        image.src = replaceUrl(image_url);
+        image.src = replaceUrl(scr_image_url);
 
-        image.onload = function() {
-            if (this.width/this.height > 1) {
-                let margin = (1 - this.width/this.height)*50;
-                this.style = 'margin-left: ' + margin + '%';
-            }
-        }
-           
-        //  title
-        let scr_title = scr_entry.querySelector('.post__title>a').innerHTML;
+        centerImage(image);
+
+        //  info
         preview.querySelector('.ep-title').innerHTML = scr_title.split(':')[1];
         preview.querySelector('.ep-number').innerHTML = scr_title.split(':')[0];
-
-        //  date
-        let scr_date = scr_entry.querySelector('.post__date').innerHTML;
         preview.querySelector('.ep-date').innerHTML = scr_date;
+        preview.querySelector('.ep-desc--small').innerHTML = scr_description;
 
-        //  description
-        let scr_desc = scr_entry.querySelector('.post__description').innerHTML;
-        if (scr_desc.length > 100) scr_desc = scr_desc.substring(0, 100) + '...';
+        //  link to episode
+        preview.querySelector('a').href = '#' + scr_url;
 
-        preview.querySelector('.ep-desc--small').innerHTML = scr_desc;
-
-        let url = scr_entry.querySelector('.post__content a').href;
-        url = url.split('/episode/')[1];
-        url = url.split('/')[0];
-
-        //  set last episode number in the input hidden value
+        //  set the last episode number in the input hidden value
         if (scr_entry == scr_list[0] && pagination == '') {
-            document.getElementById('last-episode').value = url;
+            document.getElementById('last-episode').value = scr_url;
         }
-
-        preview.querySelector('a').href = '#' + url;
 
         container.append(preview);
     });
+
+    bannerAnimation();
 }
 
 //  page animations and interaction
@@ -316,8 +257,7 @@ async function renderList(pagination) {
 function bannerAnimation() {
 
     setInterval(() => {
-        let bg = document.querySelector('.bg-image');
-        toggleActive(bg);
+        toggleActive( document.querySelector('.bg-image') );
     }, 3000);
 }
 
@@ -332,7 +272,7 @@ function parallaxAnimation(end) {
         },
 
         y: (index, target) =>
-        ScrollTrigger.maxScroll(window) * target.dataset.parallax,
+            ScrollTrigger.maxScroll(window) * target.dataset.parallax,
         ease: "none"
     });
 }
@@ -340,25 +280,19 @@ function parallaxAnimation(end) {
 function toggleDescription() {
 
     let toggle = document.getElementById('toggle-desc');
-    toggle.addEventListener('click', function () {
 
+    toggle.addEventListener('click', function () {
         let desc = document.getElementById('ep-desc');
         toggleActive(desc);
-
-        if (isActive(desc)) {
-            toggle.innerHTML = 'Read less';
-        } else {
-            toggle.innerHTML = 'Read more';
-        }
-
+        this.innerHTML = isActive(desc) ? 'Read less' : 'Read more';
     });
 }
 
-//  utility functions
+//  utilities
 
 function isActive(elem) {
 
-    if (elem.classList.contains('active')){
+    if (elem.classList.contains('active')) {
         return true
     } else {
         return false;
@@ -374,4 +308,23 @@ function toggleActive(elem) {
     }
 }
 
-function replaceUrl(url) { return TARGET_URL + url.trim(); }
+function centerImage(img) {
+
+    img.onload = function () {
+        if (this.width / this.height > 1) {
+            let margin = (1 - this.width / this.height) * 50;
+            this.style = 'margin-left: ' + margin + '%';
+        }
+    }
+}
+
+function limitParagraph(p, limit) {
+
+    if (p.length > limit) {
+        return p.substring(0, limit) + '...';
+    } else {
+        return p;
+    }
+}
+
+function replaceUrl(url) { return target_url + url.trim(); }
